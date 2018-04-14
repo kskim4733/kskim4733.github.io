d8ac7def301bb5f649689887352f305689c35fe3
kskim4733 <kskim4733@gmail.com>
2018-04-14 16:45:41 -0400

    buttons do something now

---
+++ buttonActions.js
@@ -0,0 +1,12 @@
+function openGit() {
+	window.open("https://github.com/kskim4733");
+}
+
+function openLinkedin() {
+	window.open("https://www.linkedin.com/in/kyle-kim-73373abb/");
+}
+
+function openEmail() {
+	window.location.href = "mailto:kskim4733@gmail.com"
+}
+
--- rMenu.js
+++ rMenu.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function responsiveToggle() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
